a96ce0df3df4f0bb75a9611a6c10ec2a1321617e
tomatoweb-net <marco@tomatoweb.net>
2023-03-24 10:28:11 +0100

    Add scaffold for Pins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_23_210320) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_24_084431) do
+  create_table "pins", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
